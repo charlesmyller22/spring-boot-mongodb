bdd84274235368c4e674c0421fb2790de038ccaa
Charles Pereira <charlesmyller@hotmail.com>
2023-03-19 11:03:32 +0100

    Project using Springboot and Mongodb

---
+++ src/test/java/com/charlespereira/springmongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlespereira.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
